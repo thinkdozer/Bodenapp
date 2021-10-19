1f9d8d8a9133cb708d9ebc0d899d9a97ae03d9db
haeae <64465817+haeae@users.noreply.github.com>
2021-10-19 22:17:25 +0200

    added types and fallback values

--- values.js
+++ values.js
@@ -1,13 +1,17 @@
 function getvalues(){
 let x = {
     "boden": {
-      "boden::Sand": "20",
-      "boden::lehmSand": "21",
-      "boden::Schluff": "22",
-      "boden::schluffLehm": "23",
-      "boden::Lehm": "24",
-      "boden::liteTon": "25",
-      "boden::Ton": "26",
+      "type": "radio",
+      "fallback": "10",
+      "assigns":{
+        "boden::Sand": "20",
+        "boden::lehmSand": "21",
+        "boden::Schluff": "22",
+        "boden::schluffLehm": "23",
+        "boden::Lehm": "24",
+        "boden::liteTon": "25",
+        "boden::Ton": "26",
+      }
     }
   };
 return x;
