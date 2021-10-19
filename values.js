424e11b0a75e7be2b93ccddf0095020a143cbae3
haeae <64465817+haeae@users.noreply.github.com>
2021-10-19 14:25:30 +0200

    new json import method and js calculation from json database

---
+++ values.js
@@ -0,0 +1,14 @@
+function getvalues(){
+let x = {
+    "boden": {
+      "boden::Sand": "20",
+      "boden::lehmSand": "21",
+      "boden::Schluff": "22",
+      "boden::schluffLehm": "23",
+      "boden::Lehm": "24",
+      "boden::liteTon": "25",
+      "boden::Ton": "26",
+    }
+  };
+return x;
+}
